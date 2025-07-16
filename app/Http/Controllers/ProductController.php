14fc25615bc45ecabc940e15169413d1615f92cb
David <gelmut@praweb.ru>
2025-07-16 13:24:50 +0500

    Выводим список продуктов. Оборачивем его в ресурс. Возвращем в json формате.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        return 'index';
+        $products = Product::all();
+        $products = ProductResource::collection($products);
+        return response()->json($products);
     }
 }
