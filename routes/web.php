b9d714593030cda964f7bd1660c01446f5df04a1
David <gelmut@praweb.ru>
2025-07-16 13:03:44 +0500

    Добавление маршрутов, контроллеров, временное отключение проверки csrf для маршрутов /create-order и /approve-order

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/catalog', [\App\Http\Controllers\ProductController::class, 'index']);
+Route::post('/create-order', [\App\Http\Controllers\OrderController::class, 'create'])->withoutMiddleware([
+    VerifyCsrfToken::class,
+]);
+Route::post('/approve-order',  [\App\Http\Controllers\OrderController::class, 'approved'])->withoutMiddleware([
+    VerifyCsrfToken::class,
+]);;
